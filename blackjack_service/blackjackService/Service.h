6337f9660aca46af8c5cbe4474f5e6d07a61db4f
MotmansS <57004612+MotmansS@users.noreply.github.com>
2021-06-25 12:31:38 +0200

    Delete blackjack_service directory

--- blackjack_service/blackjackService/Service.h
+++
@@ -1,9 +0,0 @@
-#ifndef _SERVICE_H_
-#define _SERVICE_H_
-#include <stdio.h>
-#include <stdlib.h>
-#include <zmq.h>
-#include <string.h>
-int drawCard(int *score,int *addition );
-int playBlackjack(void *responder, int *score, int *addition);
-#endif
